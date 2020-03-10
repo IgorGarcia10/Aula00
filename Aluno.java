ee17fc8cf413b731d5d6e1739b38f1ed95541b46
Igor Garcia <alexandre.igor64@gmail.com>
2020-03-10 20:29:55 -0300

    gets

--- Aluno.java
+++ Aluno.java
@@ -1,4 +1,13 @@
 public class Aluno {
 	private String nome;
 	private int id;
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
 }
+
