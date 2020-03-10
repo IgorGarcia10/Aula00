a35c78e53499a89acdc23373f963e3845e9cf88b
Igor Garcia <alexandre.igor64@gmail.com>
2020-03-10 20:40:16 -0300

    construtor s/ this

--- Aluno.java
+++ Aluno.java
@@ -2,12 +2,21 @@ public class Aluno {
 	private String nome;
 	private int id;
 	
+	public Aluno(String nome, int id){
+		nome = nome;
+		id = id;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
-	
+	public int getId() {
+		return id;
+	}
 	public void setId(int id) {
 		this.id = id;
 	}
 }
-
